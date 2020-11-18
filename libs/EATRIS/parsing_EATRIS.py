dc5e10ed3d5512b854bac7ea7bd9c0c7c2679ea6
Baptiste Grenier <baptiste@bapt.name>
2020-11-18 12:31:51 +0100

    Fix more import sorting problems

--- libs/EATRIS/parsing_EATRIS.py
+++ libs/EATRIS/parsing_EATRIS.py
@@ -2,14 +2,15 @@
 
 import csv
 import os
+
 import requests
 from bs4 import BeautifulSoup
 
 
 def print_details(url, csv_filename, years):
     """
-       Parsing the scientific publications from the web site and 
-       export the list in a CSV file
+    Parsing the scientific publications from the web site and
+    export the list in a CSV file
     """
 
     item_authors = item_year = item_journal = item_doi = item_title = ""
